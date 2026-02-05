033f0623aa28b130131bd9d618eaa7cf0aab5ac1
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2026-02-05 17:03:44 +0600

    decided to not continue with this handbook going withj chatgpt/ytvideo

---
+++ d11.js
@@ -0,0 +1,17 @@
+// console.log("It is day no 11 of TypeScript");
+// console.log(typeof null); //object haha unfortunate accedients of history
+// console.log(typeof []);
+// console.log(typeof {});
+// type boolean is not required alawyas in javascript
+function userOnline(numOfUsers) {
+    if (numOfUsers) {
+        return numOfUsers + " users are online";
+    }
+    return "No users are online";
+}
+// console.log(userOnline(5));
+// interesting fatctor 
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+console.log(null === null);
+console.log(undefined === undefined);
